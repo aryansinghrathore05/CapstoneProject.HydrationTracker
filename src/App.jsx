630dc826b6621d09152367c8a04d44b20722ae70
aryansinghrathore05 <rathore.aryan7859@gmail.com>
2025-05-04 23:41:07 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/globals.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen bg-red-100">Hello</div>
+    </>
+  )
+}
+
+export default App
